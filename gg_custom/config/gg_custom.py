e802ddc129319a98b3d1e0ae5f45dc7e47ff8dd0
Sun Howwrongbum <sun@libermatic.com>
2020-07-08 08:28:21 +0000

    feat: Station

--- gg_custom/config/gg_custom.py
+++ gg_custom/config/gg_custom.py
@@ -11,7 +11,17 @@ def get_data():
                     "type": "doctype",
                     "name": "Booking Party",
                     "description": frappe._("Consignor / Consignee Details"),
+                },
+            ],
+        },
+        {
+            "label": frappe._("Setup"),
+            "items": [
+                {
+                    "type": "doctype",
+                    "name": "Station",
+                    "description": frappe._("Station"),
                 }
             ],
-        }
+        },
     ]
