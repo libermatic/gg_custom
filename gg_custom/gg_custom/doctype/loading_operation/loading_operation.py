b8c11d9612a728caf5930fd3e5f4137f4dd7760e
Sun Howwrongbum <sun@libermatic.com>
2020-07-09 16:15:41 +0000

    feat(LoadingOperation): validate  ShippingOrder

--- gg_custom/gg_custom/doctype/loading_operation/loading_operation.py
+++ gg_custom/gg_custom/doctype/loading_operation/loading_operation.py
@@ -31,6 +31,26 @@ class LoadingOperation(Document):
         ):
             self.append("on_loads", booking_order)
 
+        self.off_loads = []
+        for booking_order in frappe.get_all(
+            "Booking Order",
+            filters={
+                "docstatus": 1,
+                "status": "In Transit",
+                "destination_station": self.station,
+                "last_shipping_order": self.shipping_order,
+            },
+            fields=[
+                "name as booking_order",
+                "no_of_packages",
+                "weight_actual",
+                "goods_value",
+                "source_station",
+                "destination_station",
+            ],
+        ):
+            self.append("off_loads", booking_order)
+
     def on_submit(self):
         for bo in pluck("booking_order", self.on_loads):
             doc = frappe.get_cached_doc("Booking Order", bo)
