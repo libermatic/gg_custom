7901a04d7063b397612b79e2ca4eb964e066f880
Sun Howwrongbum <sun@libermatic.com>
2020-07-17 14:35:21 +0000

    fix: ChargeTemplate validation

--- gg_custom/gg_custom/doctype/booking_order_charge_template/booking_order_charge_template.py
+++ gg_custom/gg_custom/doctype/booking_order_charge_template/booking_order_charge_template.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# pylint:disable=no-member
 # Copyright (c) 2020, Libermatic and contributors
 # For license information, please see license.txt
 
@@ -10,6 +11,10 @@ from frappe.model.document import Document
 
 class BookingOrderChargeTemplate(Document):
     def validate(self):
+        if self.is_default:
+            self._validate_default()
+
+    def _validate_default(self):
         existing = frappe.db.exists(
             "Booking Order Charge Template",
             {"is_default": 1, "name": ("!=", self.name)},
