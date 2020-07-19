60465b684f140916db08eafcef26c78db42157e4
Sun Howwrongbum <sun@libermatic.com>
2020-07-19 08:16:15 +0000

    fix(BO): typo in variable name

--- gg_custom/gg_custom/doctype/booking_order/booking_order_dashboard.py
+++ gg_custom/gg_custom/doctype/booking_order/booking_order_dashboard.py
@@ -5,7 +5,7 @@ import frappe
 def get_data():
     return {
         "fieldname": "booking_order",
-        "non_standard_fieldnames": {"Sales Invoice": "px_booking_order"},
+        "non_standard_fieldnames": {"Sales Invoice": "gg_booking_order"},
         "transactions": [
             {"label": "Shipping", "items": ["Loading Operation"]},
             {"label": "Invoicing", "items": ["Sales Invoice"]},
