4f36275d6a35b8322a08cac12622a5c4d210ca6b
Sun Howwrongbum <sun@libermatic.com>
2020-08-20 17:26:57 +0000

    feat(BP): show dashboard links

--- gg_custom/public/js/scripts/booking_party.js
+++ gg_custom/public/js/scripts/booking_party.js
@@ -34,6 +34,7 @@ export function booking_party() {
       }
       if (!frm.doc.__islocal && frm.doc.__onload) {
         erpnext.utils.set_party_dashboard_indicators(frm);
+        render_booking_order_links(frm);
       }
     },
     primary_address: function (frm) {
@@ -48,4 +49,44 @@ export function booking_party_listview_settings() {
   return {
     filters: [['disabled', '=', 0]],
   };
+}
+
+function render_booking_order_links(frm) {
+  frm.dashboard.transactions_area.empty();
+  const fields = ['consignor', 'consignee'];
+  frm.dashboard.transactions_area.append(
+    frappe.render_template('form_links', {
+      transactions: [
+        {
+          label: 'Shipping',
+          items: fields.map((field) => `Booking Order as ${field}`),
+        },
+      ],
+      internal_links: {},
+    })
+  );
+
+  function route_handler(field) {
+    return function () {
+      frappe.route_options = { [field]: frm.doc.name };
+      frappe.set_route('List', 'Booking Order', 'List');
+    };
+  }
+
+  fields.forEach(async function (field) {
+    const $link = frm.dashboard.transactions_area.find(
+      `.document-link[data-doctype='Booking Order as ${field}']`
+    );
+    $link.find('.badge-link').on('click', route_handler(field));
+    const [{ count } = {}] = await frappe.db.get_list('Booking Order', {
+      filters: { [field]: frm.doc.name },
+      fields: ['count(name) as count'],
+    });
+    if (count) {
+      $link.find('.count').text(count);
+    }
+  });
+
+  frm.dashboard.links_area.removeClass('hidden');
+  frm.dashboard.show();
 }
