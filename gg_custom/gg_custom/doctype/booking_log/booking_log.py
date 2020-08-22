230064f92dd8bd7ac672b1241ed1ab2350e309d4
Sun Howwrongbum <sun@libermatic.com>
2020-08-22 19:13:17 +0000

    feat: BookLog will stick to one unit

--- gg_custom/gg_custom/doctype/booking_log/booking_log.py
+++ gg_custom/gg_custom/doctype/booking_log/booking_log.py
@@ -1,10 +1,31 @@
 # -*- coding: utf-8 -*-
+# pylint:disable=no-member
 # Copyright (c) 2020, Libermatic and contributors
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
+
 class BookingLog(Document):
-	pass
+    def validate(self):
+        existing_loading_unit = frappe.db.get_value(
+            "Booking Log",
+            filters={
+                "name": ("!=", self.name),
+                "booking_order": self.booking_order,
+                "activity": ("!=", "Booked"),
+            },
+            fieldname="loading_unit",
+        )
+        if existing_loading_unit and self.loading_unit != existing_loading_unit:
+            frappe.throw(
+                frappe._(
+                    "Previous Loading Operation on {} has already being performed based on {}. ".format(
+                        frappe.get_desk_link("Booking Order", self.booking_order),
+                        existing_loading_unit,
+                    )
+                    + "Please execute the current one based on the same unit."
+                )
+            )
