915707f50cb31788b8494e589472eab9208836b2
Sun Howwrongbum <sun@libermatic.com>
2020-08-24 08:59:02 +0000

    feat(report): add party_name

--- gg_custom/public/js/reports/booking_accounting_ledger.js
+++ gg_custom/public/js/reports/booking_accounting_ledger.js
@@ -1,3 +1,16 @@
+async function set_party_name() {
+  const booking_party = frappe.query_report.get_filter_value('booking_party');
+  const { message: { booking_party_name } = {} } = await frappe.db.get_value(
+    'Booking Party',
+    booking_party,
+    'booking_party_name'
+  );
+  frappe.query_report.set_filter_value(
+    'booking_party_name',
+    booking_party_name
+  );
+}
+
 export function booking_accounting_ledger() {
   return {
     filters: [
@@ -21,6 +34,13 @@ export function booking_accounting_ledger() {
         fieldname: 'booking_party',
         label: 'Booking Party',
         reqd: 1,
+        on_change: set_party_name,
+      },
+      {
+        fieldtype: 'Data',
+        fieldname: 'booking_party_name',
+        label: 'Party Name',
+        read_only: 1,
       },
     ],
   };
