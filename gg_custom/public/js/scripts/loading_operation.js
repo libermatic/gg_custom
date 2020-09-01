463f2349367da40613f45a3897c86d3a6c5d7385
Sun Howwrongbum <sun@libermatic.com>
2020-09-01 16:23:50 +0000

    style(LO): exports on top

--- gg_custom/public/js/scripts/loading_operation.js
+++ gg_custom/public/js/scripts/loading_operation.js
@@ -1,64 +1,5 @@
 import sumBy from 'lodash/sumBy';
 
-function set_totals(frm) {
-  ['no_of_packages', 'weight_actual', 'goods_value'].forEach((param) =>
-    ['on_load', 'off_load'].forEach((type) => {
-      const field = `${type}_${param}`;
-      const table = frm.doc[`${type}s`] || [];
-      frm.set_value(field, sumBy(table, param));
-    })
-  );
-}
-
-function set_query_booking_order(type) {
-  return ({ station, shipping_order }) => {
-    if (!station || !shipping_order) {
-      frappe.throw(
-        __('Cannot fetch Booking Orders without Station or Shipping Order')
-      );
-      return;
-    }
-    return {
-      query: 'gg_custom.api.booking_order.query',
-      filters: { type, station, shipping_order },
-    };
-  };
-}
-
-async function set_booking_order_fields(frm, cdt, cdn) {
-  const { station, shipping_order } = frm.doc;
-  const { booking_order, loading_unit, parentfield } = frappe.get_doc(cdt, cdn);
-  const fields = ['no_of_packages', 'weight_actual', 'goods_value'];
-  function get_args() {
-    if (parentfield === 'on_loads') {
-      return { name: booking_order, station };
-    }
-    if (parentfield === 'off_loads') {
-      return { name: booking_order, shipping_order };
-    }
-  }
-  if (booking_order && loading_unit) {
-    const { message: details = {} } = await frappe.call({
-      method: 'gg_custom.api.booking_order.get_order_details',
-      args: get_args(),
-    });
-    const available =
-      loading_unit === 'Packages'
-        ? details.no_of_packages
-        : loading_unit === 'Weight'
-        ? details.weight_actual
-        : 0;
-    ['qty', 'available'].forEach((x) =>
-      frappe.model.set_value(cdt, cdn, x, available)
-    );
-    fields.forEach((x) => frappe.model.set_value(cdt, cdn, x, details[x]));
-    return;
-  }
-  [...fields, 'qty', 'available'].forEach((x) =>
-    frappe.model.set_value(cdt, cdn, x, null)
-  );
-}
-
 export function loading_operation_booking_order() {
   return {
     booking_order: set_booking_order_fields,
@@ -116,4 +57,63 @@ export function loading_operation_listview_settings() {
   return {
     filters: [['docstatus', '!=', 2]],
   };
+}
+
+function set_totals(frm) {
+  ['no_of_packages', 'weight_actual', 'goods_value'].forEach((param) =>
+    ['on_load', 'off_load'].forEach((type) => {
+      const field = `${type}_${param}`;
+      const table = frm.doc[`${type}s`] || [];
+      frm.set_value(field, sumBy(table, param));
+    })
+  );
+}
+
+function set_query_booking_order(type) {
+  return ({ station, shipping_order }) => {
+    if (!station || !shipping_order) {
+      frappe.throw(
+        __('Cannot fetch Booking Orders without Station or Shipping Order')
+      );
+      return;
+    }
+    return {
+      query: 'gg_custom.api.booking_order.query',
+      filters: { type, station, shipping_order },
+    };
+  };
+}
+
+async function set_booking_order_fields(frm, cdt, cdn) {
+  const { station, shipping_order } = frm.doc;
+  const { booking_order, loading_unit, parentfield } = frappe.get_doc(cdt, cdn);
+  const fields = ['no_of_packages', 'weight_actual', 'goods_value'];
+  function get_args() {
+    if (parentfield === 'on_loads') {
+      return { name: booking_order, station };
+    }
+    if (parentfield === 'off_loads') {
+      return { name: booking_order, shipping_order };
+    }
+  }
+  if (booking_order && loading_unit) {
+    const { message: details = {} } = await frappe.call({
+      method: 'gg_custom.api.booking_order.get_order_details',
+      args: get_args(),
+    });
+    const available =
+      loading_unit === 'Packages'
+        ? details.no_of_packages
+        : loading_unit === 'Weight'
+        ? details.weight_actual
+        : 0;
+    ['qty', 'available'].forEach((x) =>
+      frappe.model.set_value(cdt, cdn, x, available)
+    );
+    fields.forEach((x) => frappe.model.set_value(cdt, cdn, x, details[x]));
+    return;
+  }
+  [...fields, 'qty', 'available'].forEach((x) =>
+    frappe.model.set_value(cdt, cdn, x, null)
+  );
 }
