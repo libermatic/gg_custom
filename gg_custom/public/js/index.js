93cb774c5a0862e19cd8cf3c639f4d7c1e0fe53f
Sun Howwrongbum <sun@libermatic.com>
2020-09-11 15:56:29 +0000

    0.4.2

--- gg_custom/public/js/index.js
+++ gg_custom/public/js/index.js
@@ -3,7 +3,7 @@ import * as reports from './reports';
 import * as cscripts from './cscripts';
 import * as quick_entry from './quick_entry';
 
-const __version__ = '0.4.1';
+const __version__ = '0.4.2';
 
 frappe.provide('gg_custom');
 gg_custom = { __version__, scripts, reports };
