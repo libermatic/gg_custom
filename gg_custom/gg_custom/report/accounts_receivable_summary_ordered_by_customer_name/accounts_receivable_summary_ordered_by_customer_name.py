be82e207307a4f4ea9df6d45f71c9bc3e9aa3dae
Sun Howwrongbum <sun@libermatic.com>
2022-02-27 11:04:26 +0000

    feat(report): removed unnecessary columns

--- gg_custom/gg_custom/report/accounts_receivable_summary_ordered_by_customer_name/accounts_receivable_summary_ordered_by_customer_name.py
+++ gg_custom/gg_custom/report/accounts_receivable_summary_ordered_by_customer_name/accounts_receivable_summary_ordered_by_customer_name.py
@@ -2,6 +2,7 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
+import frappe
 
 from erpnext.accounts.report.accounts_receivable_summary.accounts_receivable_summary import (
     execute as accounts_receivable_summary,
@@ -9,6 +10,59 @@ from erpnext.accounts.report.accounts_receivable_summary.accounts_receivable_sum
 
 
 def execute(filters=None):
-    result = list(accounts_receivable_summary(filters))
-    result[1] = sorted(result[1] or [], key=lambda x: x.get("party_name"))
-    return result
+    result = accounts_receivable_summary(filters)
+    columns = _get_columns(result)
+    data = _get_data(result)
+    return columns, data
+
+
+def _get_columns(report):
+    columns = report[0]
+    fields = [
+        "party",
+        "party_name",
+        "invoiced",
+        "paid",
+        "outstanding",
+        "range1",
+        "range2",
+        "range3",
+        "range4",
+        "range5",
+        "total_due",
+    ]
+    return [x for x in columns if x.get("fieldname") in fields] + [
+        {
+            "label": "Booking Party",
+            "fieldname": "booking_party",
+            "fieldtype": "Link",
+            "options": "Booking Party",
+            "width": 180,
+        }
+    ]
+
+
+def _get_data(report):
+    rows = sorted(report[1] or [], key=lambda x: x.get("party_name"))
+    booking_party_by_customer_ids = (
+        {
+            x.get("customer"): x.get("name")
+            for x in frappe.get_all(
+                "Booking Party",
+                fields=["name", "customer"],
+                filters={"customer": ("in", [x.get("party") for x in rows])},
+                order_by="modified asc"
+            )
+        }
+        if rows
+        else {}
+    )
+
+    return [
+        {
+            **x,
+            "paid": x.get("advance") + x.get("paid"),
+            "booking_party": booking_party_by_customer_ids.get(x.get("party")),
+        }
+        for x in rows
+    ]
