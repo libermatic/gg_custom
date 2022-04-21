78b8e3a0bdaf717dfd9aa6f9ff84f44f7140efc9
Sun Howwrongbum <sun@libermatic.com>
2022-04-21 11:53:15 +0000

    feat: ShippingVendor

---
+++ gg_custom/gg_custom/doctype/shipping_vendor/shipping_vendor.js
@@ -0,0 +1,4 @@
+// Copyright (c) 2022, Libermatic and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Shipping Vendor', gg_custom.scripts.shipping_vendor());
---
+++ gg_custom/gg_custom/doctype/shipping_vendor/shipping_vendor_list.js
@@ -0,0 +1,2 @@
+frappe.listview_settings['Shipping Vendor'] =
+  gg_custom.scripts.shipping_vendor_listview_settings();
