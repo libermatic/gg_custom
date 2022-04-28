94c86a58d543c21926e3045d17c20d25ac75442b
Sun Howwrongbum <sun@libermatic.com>
2022-04-28 19:59:50 +0000

    feat: Shipping Order Charge Template

---
+++ gg_custom/gg_custom/doctype/shipping_order_charge_template/shipping_order_charge_template.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Libermatic and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Shipping Order Charge Template', {
+	// refresh: function(frm) {
+
+	// }
+});
