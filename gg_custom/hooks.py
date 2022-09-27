97fb7dedbcadaadc93b5ee6b3c7a4cfc65a0fa6a
Sun Howwrongbum <sun@libermatic.com>
2022-09-27 20:52:41 +0000

    refactor: use esbuild

--- gg_custom/hooks.py
+++ gg_custom/hooks.py
@@ -35,7 +35,7 @@ fixtures = [
 
 # include js, css files in header of desk.html
 # app_include_css = "/assets/gg_custom/css/gg_custom.css"
-app_include_js = ["/assets/js/gg_custom.min.js"]
+app_include_js = ["gg_custom.bundle.js"]
 
 # include js, css files in header of web template
 # web_include_css = "/assets/gg_custom/css/gg_custom.css"
