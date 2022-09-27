1dbce9cf38025afb8785f1194d5e24741d74e4b0
Sun Howwrongbum <sun@libermatic.com>
2022-09-27 20:52:11 +0000

    refactor: use pyproject

--- setup.py
+++ setup.py
@@ -1,20 +1,6 @@
-# -*- coding: utf-8 -*-
-from setuptools import setup, find_packages
+# TODO: Remove this file when bench >=v5.11.0 is adopted / v15.0.0 is released
+from setuptools import setup
 
-with open('requirements.txt') as f:
-	install_requires = f.read().strip().split('\n')
+name = "gg_custom"
 
-# get version from __version__ variable in gg_custom/__init__.py
-from gg_custom import __version__ as version
-
-setup(
-	name='gg_custom',
-	version=version,
-	description='Customizations for GG',
-	author='Libermatic',
-	author_email='info@libermatic.com',
-	packages=find_packages(),
-	zip_safe=False,
-	include_package_data=True,
-	install_requires=install_requires
-)
+setup()
