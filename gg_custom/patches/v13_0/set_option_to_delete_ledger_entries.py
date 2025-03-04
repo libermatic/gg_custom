bb7ac538d9050d4e67d70a1549d1db1f71a16072
Sun Howworngbum <sun@libermatic.com>
2025-03-04 22:01:38 +0530

    fix: deprecate set_value for singles

--- gg_custom/patches/v13_0/set_option_to_delete_ledger_entries.py
+++ gg_custom/patches/v13_0/set_option_to_delete_ledger_entries.py
@@ -2,4 +2,4 @@ import frappe
 
 
 def execute():
-    frappe.db.set_value("Accounts Settings", None, "delete_linked_ledger_entries", 1)
+    frappe.db.set_single_value("Accounts Settings", "delete_linked_ledger_entries", 1)
