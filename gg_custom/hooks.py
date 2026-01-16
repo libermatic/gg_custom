bd575551988c414bb07da8d043c4bac1319b8a4e
Sun Howworngbum <sun@libermatic.com>
2026-01-16 21:24:32 +0530

    feat: add stations to Quotation

--- gg_custom/hooks.py
+++ gg_custom/hooks.py
@@ -24,6 +24,7 @@ fixtures = [
                     "Purchase Invoice",
                     "Item",
                     "Vehicle",
+                    "Quotation",
                 ],
             ),
         },
